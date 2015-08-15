a1e77eb0fc485ba7f7c8df47728693471cddfb96
Chia <cchuang6@asu.edu>
2015-08-15 00:52:32 -0700

    change template dir to the feel_free_site

--- feel_free_site/settings.py
+++ feel_free_site/settings.py
@@ -202,8 +202,11 @@ ROOT_URLCONF = "%s.urls" % PROJECT_APP
 # or "C:/www/django/templates".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "../templates"),)
-print("TEMPLATE_DIRS", TEMPLATE_DIRS)
+templateDir = os.path.dirname(__file__)
+TEMPLATE_DIRS = (os.path.join(templateDir, "templates"),)
+print("TEMPLATE_DIRS",TEMPLATE_DIRS)
+#  TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "../templates"),)
+#  print("TEMPLATE_DIRS", TEMPLATE_DIRS)
 
 ################
 # APPLICATIONS #
