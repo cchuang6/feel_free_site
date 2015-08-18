a0d46eb885bf0d36aedc78659d48f49d06ea7884
Chia <cchuang6@asu.edu>
2015-08-18 11:28:31 -0700

    delete visualization to test

--- visualization/urls.py
+++
@@ -1,6 +0,0 @@
-from visualization import views
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='visualization'),
-                       )
