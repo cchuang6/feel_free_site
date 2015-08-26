c6617880c8886b71720d221dc9be60e08f19dcd2
Rico Lavender <rico@ricolavender.com>
2015-08-25 17:25:08 -0700

    added an image to the style2.css and added a new page called thankyou.html

--- feel_free_home/urls.py
+++ feel_free_home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='home'),
                        url(r'^dashBoard2/', views.signUpTest, name='test'),
+                       url(r'^thankyou/', views.thankyou),
+
                        )
--- feel_free_home/views.py
+++ feel_free_home/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def thankyou(request):
+    return render(request, 'thankyou.html')
+
 def signUpTest(request):
     print "yes"
     return render(request, 'index.html')
